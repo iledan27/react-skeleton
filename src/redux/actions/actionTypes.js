f7778c7c2bb530a707776ee158b48ed2714064c7
Elena Magda Placinta <magda.placinta@tss-yonder.com>
2020-03-22 22:07:54 +0200

    rename folder

--- src/redux/actions/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const LOAD_NUMBER_SUCCESS = 'LOAD_NUMBER_SUCCESS';
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import number from './homepageReducer';
-
-const rootReducer = combineReducers({
-  number
-});
-
-export default rootReducer;
